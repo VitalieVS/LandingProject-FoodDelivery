777c59c656e0c8ffd8718660df6511e9dc8c1798
VitalieVS <54939321+VitalieVS@users.noreply.github.com>
2019-11-26 13:01:21 +0200

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,29 @@
 $(document).ready(function(){
   	 AOS.init();
 
+  	 $( "#button-press" ).click(function() {
+  	 	if (valid_name() && valid_phone()) {
+  	 		//do smth
+  	 	} else{
+  	 		$( function() {
+			    $( "#dialog" ).dialog();
+			});
+  	 	}
+	});
+
   	 $(function() {
             $("#tabs").tabs();
         });
-    })
+
+});
+
+	function valid_name(){
+		let name = document.getElementById('name').value;	
+		return (name.length>=7) && (name.length<29);
+	}
+
+	function valid_phone(){
+		let phone = document.getElementById('phone').value;
+		return (phone.length = 8) || (phone.length = 9);
+	}
+

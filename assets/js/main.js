e525b7d9fa87bd026ea0ebd08350c47c30f73675
VitalieVS <54939321+VitalieVS@users.noreply.github.com>
2019-11-26 14:34:30 +0200

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,46 @@
 $(document).ready(function(){
   	 AOS.init();
 
-  	 $( "#button-press" ).click(function() {
-  	 	if (valid_name() && valid_phone()) {
-  	 		//do smth
-  	 	} else{
-  	 		$( function() {
-			    $( "#dialog" ).dialog();
-			});
+  	 $("#button-press").click(function() {  	 	
+		let name = getValue('name');
+		let phone = getValue('phone');
+		
+  	 	if (!validName(name) || !validPhone(phone)) {
+  	 		showErrors();  	 		
+  	 	} else {
+  	 		submitData();
   	 	}
 	});
 
   	 $(function() {
-            $("#tabs").tabs();
-        });
-
+        $("#tabs").tabs();
+     });
 });
-
-	function valid_name(){
-		let name = document.getElementById('name').value;	
-		return (name.length>=7) && (name.length<29);
+	
+	function validName(name){
+		return name.length >= 3 && name.length < 29 ;//&& (name.match(/\d+/g));
 	}
 
-	function valid_phone(){
-		let phone = document.getElementById('phone').value;
-		return (phone.length = 8) || (phone.length = 9);
+	function validPhone(phone){
+		return phone.length > 7 && phone.length < 10 ;// && !(phone.match(/^[A-Za-z]+$/));
+	}
+	
+	function getValue(fieldName) {
+		return document.getElementById(fieldName).value
 	}
 
+	function submitData() {
+		$("#call-form.login-box.btn").css("background-color: red;");
+	}
+
+	function showErrors(){
+		$("#dialog").dialog();
+	}
+
+
+
+
+//a = "fgdfgfdr5t4353454";
+
+//b = (a.length >= 3 && a.length < 29 && (a.search(/\d+/g)));
+//alert(b); //8
